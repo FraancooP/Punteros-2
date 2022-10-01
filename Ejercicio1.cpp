bfb5d3f8a1497330d08e4452382af0981d6127ed
FraancooP <franco_he_2011_@hotmail.com>
2022-10-01 20:41:43 -0300

    Primer commit

---
+++ Ejercicio1.cpp
@@ -0,0 +1,14 @@
+/*Desarrolle un programa que declare e inicialice un vector de 5 valores (4.5,8,2.3,1.1,3). 
+Luego, se pide que calcule y muestre la sumatoria y el promedio. 
+Utilice punteros para realizar el ejercicio.*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+}
+
