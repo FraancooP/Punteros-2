54b27c726cdf81378c51e59cff7b15881195b4f8
FraancooP <franco_he_2011_@hotmail.com>
2022-10-01 21:15:36 -0300

    Agregue ejercicio 4

---
+++ Ejercicio4.cpp
@@ -0,0 +1,21 @@
+/*Se tiene un vector de 10 elementos con valores numéricos enteros . 
+Luego crear una funcion que muestre los valores que están en posiciones impares,
+ y otra funcion con las posiciones pares.*/
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]){
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
